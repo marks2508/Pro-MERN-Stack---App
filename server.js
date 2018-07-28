3b5316401a3ca2fd3a6403520021de142b6e1df0
Mark S <markscicluna@hotmail.com>
2018-07-28 17:04:24 +0100

    chapter 6 finished and all working

--- server.js
+++ server.js
@@ -1,26 +1,21 @@
+'use strict';
+
 const express = require('express');
 const bodyParser = require('body-parser');
-const app = express();
+const MongoClient = require('mongodb').MongoClient;
 
+const app = express();
 app.use(express.static('static'));
 app.use(bodyParser.json());
 
-const issues = [
-  {
-    id: 1, status: 'Open', owner: 'Ravan',
-    created: new Date('2016-08-15'), effort: 5, completionDate: undefined,
-    title: 'Error in console when clicking Add'
-  },
-  {
-    id: 2, status: 'Assigned', owner: 'Eddie',
-    created: new Date('2016-08-16'), effort: 14, completionDate: new Date('26-08-30'),
-    title: 'Missing borrom border on panel'
-  }
-];
-
 app.get('/api/issues', (req, res) => {
-  const metadata = {total_count: issues.length};
-  res.json({_metadata: metadata, records: issues});
+  db.collection('issues').find().toArray().then(issues => {
+    const metadata = { total_count: issues.length };
+    res.json({ _metadata: metadata, records: issues });
+  }).catch(error => {
+    console.log(error);
+    res.status(500).json({ message: `Internal Server Error: ${error}` });
+  });
 });
 
 const validIssueStatus = {
@@ -33,10 +28,9 @@ const validIssueStatus = {
 };
 
 const issueFieldType = {
-  id: 'required',
   status: 'required',
   owner: 'required',
-  effort: 'required',
+  effort: 'optional',
   created: 'required',
   completionDate: 'optional',
   title: 'required'
@@ -58,18 +52,30 @@ function validateIssue(issue) {
 
 app.post('/api/issues', (req, res) => {
   const newIssue = req.body;
-  newIssue.id = issues.length + 1;
   newIssue.created = new Date();
   if (!newIssue.status)
     newIssue.status = 'New';
   const err = validateIssue(newIssue);
   if (err) {
     res.status(422).json({ message: `Invalid request: ${err}` });
+    return;
   }
-  issues.push(newIssue);
-  res.json(newIssue);
+  db.collection('issues').insertOne(newIssue).then(result =>
+    db.collection('issues').find({ _id: result.insertedId }).limit(1).next()
+  ).then(newIssue => {
+    res.json(newIssue);
+  }).catch(error => {
+    console.log(error);
+    res.status(500).json({ message: `Internal Server Error: ${error}`});
+  });
 });
 
-app.listen(3000, () => {
-  console.log('App started on port 3000');
+let db;
+MongoClient.connect('mongodb://localhost/issuetracker').then(connection => {
+  db = connection;
+  app.listen(3000, () => {
+    console.log('App started on port 3000');
+  });
+}).catch(error => {
+  console.log('ERROR:', error);
 });
